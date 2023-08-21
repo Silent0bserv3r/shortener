79d3e8769b5d0b8a44bedb26a3aaa5df7ef71f6b
Gaurav Chaubey <gauravchaubey807@gmail.com>
2023-08-21 13:25:46 +0530

    Base Changes
    Deleting unnecessary files and imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
 import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="h-screen w-screen flex justify-center items-center">
+        <main className="h-screen w-screen flex justify-center items-center gap-16">
             <div>This is a shortner app</div>
             <Link href={"/auth"}>Authorize</Link>
         </main>
