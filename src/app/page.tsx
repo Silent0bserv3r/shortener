e5de38eb1d0d07b811060982f44a3b70e29f7bf9
Gaurav Chaubey <gauravchaubey807@gmail.com>
2023-08-23 12:26:55 +0530

    Removed state persistence over pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,4 @@
-"use client";
-
 import Link from "next/link";
-import { wrapper } from "@/store/store";
-import { setAuthState } from "@/store/authSlice";
-
-export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ params }) => {
-    store.dispatch(setAuthState(false));
-    console.log("State on server", store.getState());
-    return {
-        props: {
-            authState: false,
-        },
-    };
-});
 
 export default function Home() {
     return (
