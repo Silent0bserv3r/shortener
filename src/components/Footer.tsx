78724daa17a71573d70bc0e08dbb48287e67f4c2
Gaurav Chaubey <gauravchaubey807@gmail.com>
2023-09-03 06:40:52 +0530

    Updated footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer>
-            <div>© 2023 - Shortener</div>
+        <footer className="flex w-full border-t-2 justify-center items-center p-12">
+            <div className="px-6 border-r-2 border-slate-800 font-bold">© 2023 - Shortener</div>
             <nav>
-                <ul>
-                    <li>
+                <ul className="flex text-blue-500">
+                    <li className="px-6 border-r-2 border-slate-800 hover:text-blue-800">
                         <Link href={"/"}>Terms And Conditions</Link>
                     </li>
-                    <li>
+                    <li className="px-6 border-r-2 border-slate-800 hover:text-blue-800">
                         <Link href={"/"}>Privacy</Link>
                     </li>
-                    <li>
+                    <li className="px-6 hover:text-blue-800">
                         <Link href={"/"}>Unshorten</Link>
                     </li>
                 </ul>
